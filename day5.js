e182962b6b9708f82ddfee217273128f1d14c4ff
maahin2005 <mahin.malek2005@gmail.com>
2023-07-12 14:48:35 +0530

    My JavaScript Practice Projects

---
+++ day5.js
@@ -0,0 +1,22 @@
+// Date : 10th July, 2023; Day : Monday
+// day 5 : problem 1 : Create a function that reverses an array.
+
+let arr = [1, 2, 3, 4, 5];
+
+for (let i = 4; i >= 0; i--) {
+  console.log("reverses an array :", arr[i]);
+}
+
+console.log(arr);
+
+console.log(" ");
+
+// day 5 : problem 2 : Sort an array from lowest to highest.
+
+let arr2 = [6, 7, 87, 53, 5, 8, 4, 20, 29, 55, 66, 77, 99, 102, 256, 88];
+
+arr2.sort(function (a, b) {
+  return a - b;
+});
+
+console.log("=> array from lowest to highest:", arr2);
