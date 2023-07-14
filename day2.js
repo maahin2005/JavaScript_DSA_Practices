8e43aab2a296c34fe2341abf6f9d7211ba454c95
Maahi <139224311+maahin2005@users.noreply.github.com>
2023-07-14 10:17:42 +0530

    Day-2 for JavaScript Problem solving.

---
+++ day2.js
@@ -0,0 +1,8 @@
+// Date : 7th July, 2023; Day : Friday
+// day 2 : problem 1 : Use setInterval & print the value of the variables.
+
+let sum = (a, b) => {
+  console.log("a + b =>", a + b);
+};
+
+setInterval(sum, 3000, 3, 4);
