648c4da1ee78a45c2eb4489e42b0828786d146dc
maahin2005 <mahin.malek2005@gmail.com>
2023-10-13 13:40:57 +0530

    some advance changings

--- prob3.js
+++ prob3.js
@@ -1,7 +1,7 @@
 // Merging arrays with While Loops
 
-let arr1 = [1, 3, 5, 7, 9, 11, 13, 15, 15];
-let arr2 = [0, 2, 4, 6, 8, 10, 12, 14, 16];
+let arr1 = [1, 3, 5, 7, 9, 11, 13, 15, 15, 17, 19];
+let arr2 = [0, 2, 4, 6, 8, 10, 12, 14, 16, 16, 18, 20];
 let arr = [];
 
 let a1 = 0;
@@ -24,7 +24,7 @@ while (a1 < arr1.length && a2 < arr2.length) {
 // if both array's length are not equal so do one more step.
 
 while (a1 < arr1.length || a2 < arr2.length) {
-  if (a1 < arr1.length && arr1[a1] < arr2[a2]) {
+  if (a1 < arr1.length || arr1[a1] < arr2[a2]) {
     arr[a] = arr1[a1];
 
     a1++;
