f1f5df8d2776acb9b9837a5496a0cdf70c9f2f93
maahin2005 <mahin.malek2005@gmail.com>
2023-10-16 14:23:42 +0530

    Learn Indirect Recursion

---
+++ prob7.js
@@ -0,0 +1,19 @@
+// Indirect Recursion
+
+let money = 100;
+let totalApple = 0;
+
+function buyApple(x) {
+  if (x > 0) {
+    console.warn("I have", x, "RS", "& Apples :-", totalApple);
+    buyMore(x - 10);
+  } else {
+    console.warn("I don't have more money", "& Apples :-", totalApple);
+  }
+}
+function buyMore(x) {
+  totalApple++;
+  buyApple(x - 5);
+}
+
+buyApple(money);
